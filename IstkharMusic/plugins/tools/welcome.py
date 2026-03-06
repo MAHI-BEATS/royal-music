cfe9262fa2b5d80e4fe10ff91a87bcf20c71ab19
istu56 <githubop94@gmail.com>
2026-03-06 15:36:35 +0530

    Update welcome.py

--- IstkharMusic/plugins/tools/welcome.py
+++ IstkharMusic/plugins/tools/welcome.py
@@ -1,86 +1,200 @@
-import asyncio
-from logging import getLogger
-
-from pyrogram import filters, enums
-from pyrogram.types import ChatMemberUpdated, Message
-
 from IstkharMusic import app
+from IstkharMusic.utils.database import get_assistant
+
+from pyrogram import Client, filters, enums
+from pyrogram.types import (
+    ChatMemberUpdated,
+    ChatJoinRequest,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    Message,
+)
+
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageChops
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+
+import asyncio
+import random
+import os
+from logging import getLogger
 
 LOGGER = getLogger(__name__)
 
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
+db = mongo["Istkhar_DB"]
+welcomedb = db["welcome_toggle_system"]
 
-class WelDatabase:
-    def __init__(self):
-        self.data = {}
+async def get_welcome(chat_id: int):
+    data = await welcomedb.find_one({"chat_id": chat_id})
+    if not data:
+        return True
+    return data.get("welcome", True)
 
-    async def find_one(self, chat_id):
-        return chat_id in self.data
+async def enable_welcome(chat_id: int):
+    await welcomedb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"welcome": True}},
+        upsert=True
+    )
 
-    async def add_wlcm(self, chat_id):
-        if chat_id not in self.data:
-            self.data[chat_id] = True
+async def disable_welcome(chat_id: int):
+    await welcomedb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"welcome": False}},
+        upsert=True
+    )
 
-    async def rm_wlcm(self, chat_id):
-        if chat_id in self.data:
-            del self.data[chat_id]
+class temp:
+    MELCOW = {}
+
+def circle(pfp, size=(390, 390), brightness_factor=1.4):
+    pfp = pfp.resize(size).convert("RGBA")
+    pfp = ImageEnhance.Brightness(pfp).enhance(brightness_factor)
+
+    mask = Image.new("L", size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, size[0], size[1]), fill=255)
+
+    pfp.putalpha(mask)
+    return pfp
 
 
-wlcm = WelDatabase()
+def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+    background = Image.open("IstkharMusic/assets/shiviwel2.png").convert("RGBA")
 
-@app.on_message(filters.command("welcome") & ~filters.private)
-async def auto_state(_, message: Message):
-    usage = "**ᴜsᴀɢᴇ :-** `/welcome on / off`"
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(pfp, size=(390, 390), brightness_factor=brightness_factor)
 
-    if len(message.command) == 1:
-        return await message.reply_text(usage)
+    background.paste(pfp, (90, 115), pfp)
+    draw = ImageDraw.Draw(background)
+ 
+    output_path = f"downloads/welcome_{id}.png"
+    background.save(output_path)
 
-    user = await app.get_chat_member(message.chat.id, message.from_user.id)
+    return output_path
+ 
+@app.on_message(filters.command("welcome") & filters.group)
+async def welcome_cmd(_, message: Message):
 
-    if user.status not in (
-        enums.ChatMemberStatus.ADMINISTRATOR,
-        enums.ChatMemberStatus.OWNER,
-    ):
-        return await message.reply_text(
-            "**sᴏʀʀʏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴍᴀɴᴀɢᴇ ᴡᴇʟᴄᴏᴍᴇ!**"
-        )
+    chat = message.chat
+    chat_id = chat.id
 
-    chat_id = message.chat.id
-    state = message.text.split(None, 1)[1].lower()
-    disabled = await wlcm.find_one(chat_id)
+    user = await app.get_chat_member(chat_id, message.from_user.id)
+    if user.status not in (enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER):
+        return await message.reply_text("**» ᴏɴʟʏ ᴀᴅᴍɪɴꜱ ᴄᴀɴ ʜᴀɴᴅʟᴇ ᴡᴇʟᴄᴏᴍᴇ ꜱʏꜱᴛᴇᴍ**")
 
-    if state == "off":
-        if disabled:
-            return await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ!**")
-        await wlcm.add_wlcm(chat_id)
-        await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ᴅɪsᴀʙʟᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.**")
+    state = await get_welcome(chat_id)   
+    status = "ᴇɴᴀʙʟᴇᴅ" if state else "ᴅɪꜱᴀʙʟᴇᴅ"
 
-    elif state == "on":
-        if not disabled:
-            return await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ!**")
-        await wlcm.rm_wlcm(chat_id)
-        await message.reply_text("**ᴡᴇʟᴄᴏᴍᴇ ᴇɴᴀʙʟᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.**")
+    btn = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton("ᴇɴᴀʙʟᴇ", callback_data=f"wlc_on_{chat_id}"),
+            InlineKeyboardButton("ᴅɪꜱᴀʙʟᴇ", callback_data=f"wlc_off_{chat_id}")
+        ]
+    ])
 
+    await message.reply_text(
+        f"» ᴄᴜʀʀᴇɴᴛʟʏ ᴡᴇʟᴄᴏᴍᴇ ꜱᴛᴀᴛᴜꜱ **{status}** ɪɴ **{chat.title}**",
+        reply_markup=btn
+    )
+
+@app.on_callback_query(filters.regex("wlc_"))
+async def welcome_toggle(_, query):
+
+    data = query.data.split("_")
+    action = data[1]
+    chat_id = int(data[2])
+
+    member = await app.get_chat_member(chat_id, query.from_user.id)
+    if member.status not in (enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER):
+        return await query.answer("ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀᴅᴍɪɴ ʙᴀʙʏ 🥺", show_alert=True)
+
+    if action == "on":
+        await enable_welcome(chat_id)
+        new_status = "ᴇɴᴀʙʟᴇᴅ"
     else:
-        await message.reply_text(usage)
+        await disable_welcome(chat_id)
+        new_status = "ᴅɪꜱᴀʙʟᴇᴅ"
+
+    chat = await app.get_chat(chat_id)
+
+    await query.message.edit_text(
+        f"» ᴡᴇʟᴄᴏᴍᴇ ᴍᴇꜱꜱᴀɢᴇ **{new_status}** ɪɴ **{chat.title}** ʙʏ :- **{query.from_user.mention}**"
+    )
+
+    await query.answer()
 
 
 @app.on_chat_member_updated(filters.group, group=-3)
 async def greet_new_member(_, member: ChatMemberUpdated):
-    chat_id = member.chat.id
-    disabled = await wlcm.find_one(chat_id)
 
-    if disabled:
+    chat_id = member.chat.id
+    is_enabled = await get_welcome(chat_id)
+    if not is_enabled:
         return
 
-    if member.new_chat_member and not member.old_chat_member:
-        user = member.new_chat_member.user
-        count = await app.get_chat_members_count(chat_id)
+    user = member.new_chat_member.user if member.new_chat_member else None
+    if not user:
+        return
 
-        text = (
-            f"**𝐖ᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ɢʀᴏᴜᴘ ʙᴀʙʏ ➻ {user.mention}**"
-        )
+    if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
 
         try:
-            await app.send_message(chat_id, text)
-        except Exception as e:
-            LOGGER.error(e)
+            pic = await app.download_media(
+                user.photo.big_file_id, file_name=f"pp{user.id}.png"
+            )
+        except:
+            pic = "IstkharMusic/assets/upic.png"
+
+        old = temp.MELCOW.get(f"welcome-{chat_id}")
+        if old:
+            try:
+                await old.delete()
+            except:
+                pass
+
+        welcomeimg = welcomepic(
+            pic,
+            user.first_name,
+            member.chat.title,
+            user.id,
+            user.username
+        )
+
+        msg = await app.send_photo(
+            chat_id,
+            photo=welcomeimg,
+            caption=f"""
+**⏤͟͟͞͞★ ʜᴇʟʟᴏ ᴅᴇᴀʀ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ : {member.chat.title}**
+
+<u>**❖ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏ**</u>
+
+**➻ ɴᴀᴍᴇ »** {user.mention}
+**➻ ᴄʜᴀᴛ_ɪᴅ »** `{user.id}`
+**➻ ᴜ_ɴᴀᴍᴇ »** @{user.username}
+   
+**➻ ᴛʜᴀɴᴋs ғᴏʀ ᴊᴏɪɴɪɴɢ ᴜs ⚡️~!
+❅─────✧❅✦❅✧─────❅**
+""",
+            reply_markup=InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton(
+                        "⊚ ᴧᴅᴅ ᴍᴇ ᴛᴏ ʏᴏυʀ ᴄʜᴧᴛ ⊚",
+                        url=f"https://t.me/{app.username}?startgroup=true"
+                    )
+                ]
+            ])
+        )
+
+        async def delete_welcome():
+            await asyncio.sleep(30)
+            try:
+                await msg.delete()
+                if f"welcome-{chat_id}" in temp.MELCOW:
+                    del temp.MELCOW[f"welcome-{chat_id}"]
+            except:
+                pass
+
+        asyncio.create_task(delete_welcome())
+        temp.MELCOW[f"welcome-{chat_id}"] = msg
