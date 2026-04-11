3ec554192e175b0b5053929e8588aa405d5f2c76
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-11 12:55:20 +0530

    Update __init__.py

--- IstkharMusic/__init__.py
+++ IstkharMusic/__init__.py
@@ -2,18 +2,19 @@ import logging
 import time
 from pyrogram import Client
 
-# We import Istu here but rename it to 'app' so __main__.py is happy!
-from IstkharMusic.core.bot import Istu as app
+from IstkharMusic.core.bot import Istu
 from IstkharMusic.core.git import git
-from IstkharMusic.core.dir import dirr  # <-- Changed to dirr
+from IstkharMusic.core.dir import dirr
 from IstkharMusic.core.userbot import Userbot
 from IstkharMusic.misc import dbb
 from IstkharMusic.logging import LOGGER
 
-dirr()  # <-- Changed to dirr()
+dirr()
 git()
 dbb()
 
+# Here is the fix! We actually build the bot instance here.
+app = Istu()
 userbot = Userbot()
 
 from .platforms import *
