2984d6ac78a7c739cff35fef165523c06105e3ff
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-11 11:16:15 +0530

    Update __init__.py

--- IstkharMusic/__init__.py
+++ IstkharMusic/__init__.py
@@ -1,8 +1,14 @@
-from IstkharMusic.core.dir import dirr
+# --- Top of file ---
+import logging
+import time
+from pyrogram import Client
+# ... other basic setup imports ...
+
+# --- Bottom of file ---
+from IstkharMusic.core.bot import Istu
 from IstkharMusic.core.git import git
 from IstkharMusic.core.userbot import Userbot
 from IstkharMusic.misc import dbb
-from .logging import LOGGER
 
 dirr()
 git()
