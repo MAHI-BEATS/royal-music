cae1c6286825aee7746629787541555f4256e041
SHIV <74583297+shivashish44@users.noreply.github.com>
2026-04-11 12:03:54 +0530

    Update __init__.py

--- IstkharMusic/__init__.py
+++ IstkharMusic/__init__.py
@@ -1,21 +1,19 @@
-# --- Top of file ---
 import logging
 import time
 from pyrogram import Client
-# ... other basic setup imports ...
 
-# --- Bottom of file ---
-from IstkharMusic.core.bot import Istu
+from IstkharMusic.core.bot import app
 from IstkharMusic.core.git import git
+from IstkharMusic.core.dir import dir
 from IstkharMusic.core.userbot import Userbot
 from IstkharMusic.misc import dbb
+from IstkharMusic.logging import LOGGER
 
 dir()
 git()
 dbb()
 
 userbot = Userbot()
-
 
 from .platforms import *
 
@@ -26,4 +24,3 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-
